f06f99ac1309cfe1b0465b64b7b9b77ee904d6fc
yofikingstonp111 <yofikingston@gmail.com>
2022-03-22 16:43:23 +0700

    Web_form_flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
+
 
 #Flask application instance
--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-#secret key configuration
+#class congfig
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'youl-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATION = False
+
