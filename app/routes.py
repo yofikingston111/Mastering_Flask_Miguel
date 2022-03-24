7955a84e7053978bd163644122ea4620e7f9a00c
yofikingstonp111 <yofikingston@gmail.com>
2022-03-25 02:08:18 +0700

    Web_form_flask

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,39 @@
 from flask import render_template
 from app import app
+from flask_login import current_user, login_user
+from app.models import User
 from app.forms import LoginForm
 from flask import render_template, flash, redirect, url_for
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
 
-#Home page route
 
 @app.route('/login', methods=['GET', 'POST'])
-
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, dont missed '.format( form.username.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign in', form=form)
 
-@app.route('/')
-def index():
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
 
+# route
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    return render_template("index.html", title='Home Page', posts=posts)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
--- microblog.py
+++ microblog.py
@@ -1,5 +1,9 @@
-from app import app
+from app import app, db
+from app.models import User, Post
 
+@app.shell_context_processors
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post':Post}
 #Main application module
 #.flaskenv: Environment variables for flask command
 
