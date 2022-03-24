12dd25ec67067cfca66385c3c4926a510f039b71
yofikingstonp111 <yofikingston@gmail.com>
2022-03-24 15:22:26 +0700

    Web_form_flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from config import Config
-from flask_postgre import PostgreSQL
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- config.py
+++ config.py
@@ -3,7 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 #class congfig
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    # SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost/UserDB'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
 
