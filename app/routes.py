0fb8ddaa4e89a1b960ef9c076d6fd0f7dd088bde
yofikingstonp111 <yofikingston@gmail.com>
2022-03-25 11:19:11 +0700

    Web_form_flask

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,3 @@
-from flask import render_template
 from app import app
 from flask_login import current_user, login_user
 from app.models import User
@@ -8,6 +7,8 @@ from flask_login import logout_user
 from flask_login import login_required
 from flask import request
 from werkzeug.urls import url_parse
+from app import db
+from app.forms import RegistrationForm
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -21,16 +22,26 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect('next_page')
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign in', form=form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulation, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
-# route
-@app.route('/')
-@app.route('/index')
-@login_required
-def index():
-    return render_template("index.html", title='Home Page', posts=posts)
 
 
 @app.route('/logout')
