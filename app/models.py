fc9b8e661759ed84d29e04cf17323771729697a5
yofikingstonp111 <yofikingston@gmail.com>
2022-03-27 01:12:53 +0700

    Web_form_flask

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 from flask_login import UserMixin
 from app import login
+
 
 
 class User(UserMixin, db.Model):
@@ -17,7 +19,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size
+        )
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,15 @@ from app.forms import RegistrationForm
 def index():
     return render_template('index.html')
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author' :user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
