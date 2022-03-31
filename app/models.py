d6637c64bdc38db7168c7ed874ec29350f2d656f
yofikingstonp111 <yofikingston@gmail.com>
2022-04-01 01:33:30 +0700

    Web_form_flask

--- app/models.py
+++ app/models.py
@@ -5,7 +5,10 @@ from hashlib import md5
 from flask_login import UserMixin
 from app import login
 
-
+followers = db.Table('followers',
+     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+     )
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,33 +19,29 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
-    followers = db.Table('followers',
-     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-     )
+
     followed = db.relationship(
     'User', secondary=followers,
     primaryjoin=(followers.c.follower_id == id),
     secondaryjoin=(followers.c.followed_id == id),
     backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
-    def follow(self, user):
-        if not self.is_following(user):
-            self.followed.append(user)
+    def follow(self, User):
+        if not self.is_following(User):
+            self.followed.append(User)
 
-    def unfollow(self, user):
-        if self.is_following(user):
-            self.followed.remove(user)
+    def unfollow(self, User):
+        if self.is_following(User):
+            self.followed.remove(User)
 
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
-
-    # def followed_posts(self):
-    #     return Post.query.join(
-    #         followers, (followers.c.followed_id == Post.user_id)).filter(
-    #             followers.c.follower_id == self.id).order_by(
-    #                 Post.timestamp.desc())
+    def followed_posts(self):
+        return Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id).order_by(
+                    Post.timestamp.desc())
 
 
     def set_password(self, password):
