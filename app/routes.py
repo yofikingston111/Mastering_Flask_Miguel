02c4aa854a2980405da72a641e0957f023f59122
yofikingstonp111 <yofikingston@gmail.com>
2022-04-03 02:04:10 +0700

    Web_form_flask

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask_login import current_user, login_user
-from app.models import User
+from app.models import User, Post
 from app.forms import LoginForm
 from flask import render_template, flash, redirect, url_for
 from flask_login import logout_user
@@ -39,10 +39,8 @@ def edit_profile():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    posts = Post.query.all()
+
     return render_template('user.html', user=user, posts=posts)
 
 
