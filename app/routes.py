704f265b0a916bf3ebb26a3b0529028ddf652316
yofikingstonp111 <yofikingston@gmail.com>
2022-04-29 17:10:51 +0700

    Web_form_flask

--- app/routes.py
+++ app/routes.py
@@ -17,7 +17,6 @@ from app.forms import PostForm
 
 
 
-
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
@@ -28,6 +27,7 @@ def index():
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
+
         return redirect(url_for('index'))
 
     posts = [
@@ -44,6 +44,7 @@ def index():
     return render_template("index.html", title='Home Page', form=form,
                             posts=posts)
 
+
 #explore function
 @app.route('/explore')
 @login_required
@@ -51,9 +52,9 @@ def explore():
     posts = Post.query.order_by(Post.timestamp.desc()).all()
     return render_template('index.html', title='Explore', posts=posts)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
@@ -158,6 +159,7 @@ def user(username):
 
     form = EmptyForm()
     return render_template('user.html', user=user, posts=posts, form=form)
+
 
 @app.route('/logout')
 def logout():
