3b9fa9002b5f349164fd0316e14cb9e6a71cf33d
yofikingstonp111 <yofikingston@gmail.com>
2022-05-11 12:33:49 +0700

    Web_form_flask

--- app/routes.py
+++ app/routes.py
@@ -41,9 +41,6 @@ def index():
         }
     ]
 
-    # posts = current_user.followed_posts().all()
-    # return render_template("index.html", title='Home Page', form=form,
-    #                         posts=posts)
 
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
